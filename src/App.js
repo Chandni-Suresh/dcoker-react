3cc47b2450c7bcbb5580ee8914660bfb38a71507
chandni-suresh <chandnisuresh0224@gmail.com>
2022-03-17 23:42:30 -0700

    created a new feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Praise the Lord!
+          Praise the from Feature branch!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          HI there from Feature Branch!
+          Learn React
         </a>
       </header>
     </div>
